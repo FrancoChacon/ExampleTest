971404c2259fd6bcfdcd83d94624bcec675caab9
Franco Chacon <franco.chacon@delee.bio>
2019-07-19 13:54:45 -0500

    Add project files.

---
+++ ExampleTest/FooClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleTest
+{
+    public class FooClass
+    {
+        /// <summary>
+        /// Hola
+        /// </summary>
+        public int PublicVariable;
+
+        private int PrivateVariable;
+
+    }
+}
